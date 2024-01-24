5b9ae3d2af84b18f69e667a7afdd08c392f84d18
Camispiva <mila.piva.silva@gmail.com>
2024-01-24 08:09:07 -0300

    23/01

---
+++ Aula 1/script.sql
@@ -0,0 +1,11 @@
+ -- Criar um banco de dados
+create database lojinha; 
+
+ --Listar um banco de dados
+show databases;
+
+-- Acessar um banco de dados
+use lojinha;
+
+ -- Excluir um banco de dados
+drop database lojinha;
